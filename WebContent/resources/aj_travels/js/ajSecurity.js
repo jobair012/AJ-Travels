bd02df167b3d89ed70286d6a42553c21b3c4c793
jobair012 <jobair012@gmail.com>
2016-10-09 00:46:54 +0600

    UI related more resources

---
+++ WebContent/resources/aj_travels/js/ajSecurity.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+function submitLoginForm(){
+	var username = $("#login-username").val();
+	var password = $("#login-password").val();
+	var remember = $("#login-remember").val();
+	
+	alert(username + " " + password+ " " + remember);
+}
